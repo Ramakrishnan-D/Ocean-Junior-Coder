c91181dfb0f63ce79a55c86b42b4e410ed0fca9c
Ramakrishnan-D <krishdev94@gmail.com>
2022-05-02 12:45:51 +0530

    Ramakrishnan component included in  App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ import Team from "./MeetTeam/Team.jsx";
 import MobileApp from "./MobileAppDesign/MobileApp.jsx";
 import Contact from "./Contact/Contact.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Ramakrishnan from "./Ramakrishnan";
 
 export default function App() {
   return (
@@ -33,6 +34,7 @@ export default function App() {
       <Team />
       <Contact />
       <Footer />
+      <Ramakrishnan />
     </>
   );
 }
