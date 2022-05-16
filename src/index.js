afdc4efc15636949714cfd9878e9669b6f6c10ca
Ramakrishnan-D <krishdev94@gmail.com>
2022-05-16 13:11:17 +0530

    modified navbar homepagesection

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@ import App from "./components/App";
 let a = 0;
 window.addEventListener("scroll", () => {
   let scroll = parseInt(window.scrollY);
-  console.log(scroll);
+  // console.log(scroll);
   a = scroll;
   if (scroll > 100) {
     document.querySelector("navbar").style.position = "fixed";
     document.querySelector("navbar").style.top = "-100px";
-  }
-  if (scroll < 100) {
-    document.querySelector("navbar").style.position = "absolute";
-    document.querySelector("navbar").style.top = "0px";
   }
   if (scroll > 160) {
     document.querySelector("navbar").style.top = "0px";
@@ -23,6 +19,10 @@ window.addEventListener("scroll", () => {
   }
   if (scroll < 160) {
     document.querySelector("navbar").className = "";
+  }
+  if (scroll < 100) {
+    // document.querySelector("navbar").style.position = "fixed";
+    document.querySelector("navbar").style.top = "0px";
   }
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
